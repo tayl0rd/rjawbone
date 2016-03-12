fa0499690b8c8b78de8ee0f5a1435118e99606f8
Taylor Daugherty <mdaugherty6@gmail.com>
2016-03-12 09:17:59 -0500

    filter sensitive data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,5 +13,7 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
   config.hook_into :webmock
   config.ignore_hosts 'codeclimate.com'
+
+  config.filter_sensitive_data('<TOKEN>') { ENV['TOKEN'] }
 end
 
